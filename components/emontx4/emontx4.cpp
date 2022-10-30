5ec18e5e7ad4508b7f6d6f341e285ee0f9f5bee0
borpin <brian.orpin@gmail.com>
2022-10-30 11:41:01 +0000

    trigger work

--- components/emontx4/emontx4.cpp
+++ components/emontx4/emontx4.cpp
@@ -162,7 +162,7 @@ void Emontx4Component::parse_json_data_(){
         if (t3_sensor_ != nullptr) {
             t3_sensor_->publish_state(float(json_data["T3"])*0.1);
         }
-        Emontx4OnDataTrigger::process(Emontx4OnDataTrigger);
+        Emontx4OnDataTrigger::process(this->Emontx4OnDataTrigger);
 
         // TODO Trigger ON_DATA
     });
